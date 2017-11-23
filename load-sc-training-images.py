fc83fceba10caaaa3e01d14241e40bb527255544
LAPTOP-47ULR2KJ\chris <chrisjolly25@gmail.com>
2017-11-23 19:57:45 +1300

    Create label arrays of the same size as the image arrays. For each class, slice 80% into the training set, 10% into validation, 10% into test. Add the labels at the coresponding indexes to the train, valid, and test arrays. Shuffle each pair of arrays (eg, images and labels) using the same permutation for each member of the pair, so that the relationship between image index and label index is maintained (eg, if a teal is shuffled to position 30 of the image array, then position 30 of the label array must have a teal label). Store datasets and labels in a pickle file.

--- load-sc-training-images.py
+++ load-sc-training-images.py
@@ -17,6 +17,12 @@ from six.moves import cPickle as pickle
 image_size = 64
 pixel_depth = 255.0
 
+num_train_per_set = 800
+num_valid_per_set = 100
+num_test_per_set = 100
+
+data_root = '.'
+
 image_folder_teal = './training-data/teal-marine-wandering/'
 image_folder_red = './training-data/red-marine-wandering/'
 image_folder_dirt = './training-data/badlands-dirt/'
@@ -45,8 +51,56 @@ def load_single_image(image_path):
               pixel_depth / 2) / pixel_depth
     return image_data
 
+image_dataset_teal = load_image_folder(image_folder_teal)
+image_dataset_red = load_image_folder(image_folder_red)
+image_dataset_dirt = load_image_folder(image_folder_dirt)
 
-dataset = load_image_folder(image_folder_red)
+np.random.shuffle(image_dataset_teal)
+np.random.shuffle(image_dataset_red)
+np.random.shuffle(image_dataset_dirt)
+
+def make_arrays(nb_rows, img_size):
+  if nb_rows:
+    dataset = np.ndarray((nb_rows, img_size, img_size, 3), dtype=np.float32)
+    labels = np.ndarray(nb_rows, dtype=np.int32)
+  else:
+    dataset, labels = None, None
+  return dataset, labels
+
+train_dataset, train_labels = make_arrays(3 * num_train_per_set, image_size)
+valid_dataset, valid_labels = make_arrays(3 * num_train_per_set, image_size)
+test_dataset, test_labels = make_arrays(3* num_test_per_set, image_size)
+
+def add_labels(label_array, num_classes, num_per_class_per_set):
+    label_array[0:num_per_class_per_set] = 0
+    label_array[num_per_class_per_set:2*num_per_class_per_set] = 1
+    label_array[2*num_per_class_per_set:3*num_per_class_per_set] = 2
+
+add_labels(train_labels, 3, num_train_per_set)
+add_labels(valid_labels, 3, num_valid_per_set)
+add_labels(test_labels, 3, num_test_per_set)
+
+train_dataset[0:num_train_per_set] = image_dataset_teal[0:num_train_per_set]
+train_dataset[num_train_per_set:2*num_train_per_set] = image_dataset_red[0:num_train_per_set]
+train_dataset[2*num_train_per_set:3*num_train_per_set] = image_dataset_dirt[0:num_train_per_set]
+
+valid_dataset[0:num_valid_per_set] = image_dataset_teal[num_train_per_set:num_train_per_set + num_valid_per_set]
+valid_dataset[num_valid_per_set:2*num_valid_per_set] = image_dataset_red[num_train_per_set:num_train_per_set + num_valid_per_set]
+valid_dataset[2*num_valid_per_set:3*num_valid_per_set] = image_dataset_dirt[num_train_per_set:num_train_per_set + num_valid_per_set]
+
+test_dataset[0:num_test_per_set] = image_dataset_teal[num_train_per_set + num_valid_per_set:num_train_per_set + num_valid_per_set + num_test_per_set]
+test_dataset[num_test_per_set:2*num_test_per_set] = image_dataset_red[num_train_per_set + num_valid_per_set:num_train_per_set + num_valid_per_set + num_test_per_set]
+test_dataset[2*num_test_per_set:3*num_test_per_set] = image_dataset_dirt[num_train_per_set + num_valid_per_set:num_train_per_set + num_valid_per_set + num_test_per_set]
+
+def randomize(dataset, labels):
+  permutation = np.random.permutation(labels.shape[0])
+  shuffled_dataset = dataset[permutation,:,:]
+  shuffled_labels = labels[permutation]
+  return shuffled_dataset, shuffled_labels
+train_dataset, train_labels = randomize(train_dataset, train_labels)
+test_dataset, test_labels = randomize(test_dataset, test_labels)
+valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
+
 
 def render_image_ascii(image_data):
     for i in range (0, 64):
@@ -60,6 +114,40 @@ def render_image_ascii(image_data):
                 line = line + '.'
         print(line)
 
-for i in range(25, 45):
-    render_image_ascii(dataset[i])
+print("Train sample")
+for i in range(0, 10):
+    render_image_ascii(train_dataset[i])
     print(" ")
+
+print("Valid sample")
+for i in range(0, 10):
+    render_image_ascii(valid_dataset[i])
+    print(" ")
+
+print("Test sample")
+for i in range(0, 10):
+    render_image_ascii(test_dataset[i])
+    print(" ")
+
+pickle_file = os.path.join(data_root, 'scMarines.pickle')
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print('Unable to save data to', pickle_file, ':', e)
+  raise
+
+
+
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size)
